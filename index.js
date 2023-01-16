6e6d62288ea304f204439b1536031690ad26fd0a
Dimitar <dimikalapocev@gmail.com>
2023-01-16 22:18:14 +0100

    Installed EJS, changed back-end to support EJS, changed all .html files to .ejs files. Added routes for all the web pages that we will need for now.

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const app = express();
+
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/src/index.html');
+  res.render('index');
 });
 
-const port = 3000;
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
+app.get('/services', (req, res) => {
+  res.render('services');
+});
+
+app.get('/about-us', (req, res) => {
+  res.render('about-us');
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects');
+});
+
+app.use((req, res, next) => {
+  res.status(404).render('404');
+});
+
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 3000;
+}
 app.listen(port, () => {
-  console.log(`Server started on port ${3000}`);
+  console.log(`Server started on port ${port}`);
 });
