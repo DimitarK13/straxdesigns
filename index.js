ed1484755253edce7ae10918f572628a3183458a
Dimitar <dimikalapocev@gmail.com>
2023-01-23 09:38:32 +0100

    Added projects and backend to support it.

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
 const app = express();
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });
 
-app.get("/index", (req, res) => {
-  res.render("index");
+app.get('/index', (req, res) => {
+  res.render('index');
 });
 
-app.get("/services", (req, res) => {
-  res.render("services");
+app.get('/services', (req, res) => {
+  res.render('services');
 });
 
-app.get("/about-us", (req, res) => {
-  res.render("about-us");
+app.get('/about-us', (req, res) => {
+  res.render('about-us');
 });
 
-app.get("/projects", (req, res) => {
-  res.render("projects");
+app.get('/projects', (req, res) => {
+  res.render('projects');
 });
 
-app.get("/contact-us", (req, res) => {
-  res.render("contact-us");
+app.get('/contact-us', (req, res) => {
+  res.render('contact-us');
+});
+
+app.get('/projects/neotek-systems', (req, res) => {
+  res.render('projects/neotek-systems');
 });
 
 app.use((req, res, next) => {
-  res.status(404).render("404");
+  res.status(404).render('404');
 });
 
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (port == null || port == '') {
   port = 3000;
 }
 app.listen(port, () => {
